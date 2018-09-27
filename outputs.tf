3ab618c581dae3f84c5254e3512fa2217ff349ee
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:38 -0700

    terraform fmt after description add

--- outputs.tf
+++ outputs.tf
@@ -1,257 +1,257 @@
 # Deployed Name Prefix
 output "name_prefix" {
   description = "Cluster Name"
-  value = "${var.name_prefix}"
+  value       = "${var.name_prefix}"
 }
 
 # Deployed Bootstrap disk size
 output "bootstrap.disk_size" {
   description = "bootstrap.disk size"
-  value = "${module.bootstrap.disk_size}"
+  value       = "${module.bootstrap.disk_size}"
 }
 
 # Deployed Bootstrap disk type
 output "bootstrap.disk_type" {
   description = "bootstrap.disk type"
-  value = "${module.bootstrap.disk_type}"
+  value       = "${module.bootstrap.disk_type}"
 }
 
 # Deployed bootstrap machine type
 output "bootstrap.machine_type" {
   description = "bootstrap.machine type"
-  value = "${module.bootstrap.machine_type}"
+  value       = "${module.bootstrap.machine_type}"
 }
 
 # Deployed bootstrap image
 output "bootstrap.image" {
   description = "bootstrap.image"
-  value = "${module.bootstrap.image}"
+  value       = "${module.bootstrap.image}"
 }
 
 # Deployed bootstrap agent SSH user
 output "bootstrap.ssh_user" {
   description = "bootstrap.ssh user"
-  value = "${module.bootstrap.ssh_user}"
+  value       = "${module.bootstrap.ssh_user}"
 }
 
 # Returns the ID of the prereq script (if image are not used)
 output "bootstrap.prereq_id" {
   description = "bootstrap.prereq id"
-  value = "${module.bootstrap.prereq_id}"
+  value       = "${module.bootstrap.prereq_id}"
 }
 
 # Number of Masters
 output "num_masters" {
   description = "Specify the amount of masters. For redundancy you should have at least 3"
-  value = "${var.num_masters}"
+  value       = "${var.num_masters}"
 }
 
 # Deployed master disk size
 output "masters.disk_size" {
   description = "masters.disk size"
-  value = "${module.masters.disk_size}"
+  value       = "${module.masters.disk_size}"
 } #
 
 # Deployed master disk type
 output "masters.disk_type" {
   description = "masters.disk type"
-  value = "${module.masters.disk_type}"
+  value       = "${module.masters.disk_type}"
 }
 
 # Deployed master machine type
 output "masters.machine_type" {
   description = "masters.machine type"
-  value = "${module.masters.machine_type}"
+  value       = "${module.masters.machine_type}"
 }
 
 # Deployed master image
 output "masters.image" {
   description = "masters.image"
-  value = "${module.masters.image}"
+  value       = "${module.masters.image}"
 }
 
 # Deployed masters agent SSH user
 output "masters.ssh_user" {
   description = "masters.ssh user"
-  value = "${module.masters.ssh_user}"
+  value       = "${module.masters.ssh_user}"
 }
 
 # Master Google Front End Load Balancer Address
 output "forwarding_rules.masters" {
   description = "forwarding rules.masters"
-  value = "${module.dcos-forwarding-rules.masters_ip_address}"
+  value       = "${module.dcos-forwarding-rules.masters_ip_address}"
 }
 
 # Public Agents Google Front End Load Balancer Address
 output "forwarding_rules.public_agents" {
   description = "forwarding rules.public agents"
-  value = "${module.dcos-forwarding-rules.public_agents_ip_address}"
+  value       = "${module.dcos-forwarding-rules.public_agents_ip_address}"
 }
 
 # Returns the ID of the prereq script (if image are not used)
 output "masters.prereq_id" {
   description = "masters.prereq id"
-  value = "${module.masters.prereq_id}"
+  value       = "${module.masters.prereq_id}"
 }
 
 # Number of private agents
 output "num_private_agents" {
   description = "Specify the amount of private agents. These agents will provide your main resources"
-  value = "${var.num_private_agents}"
+  value       = "${var.num_private_agents}"
 }
 
 # Deployed private agent disk size
 output "private_agents.disk_size" {
   description = "private agents.disk size"
-  value = "${module.private_agents.disk_size}"
+  value       = "${module.private_agents.disk_size}"
 }
 
 # Deployed private agent disk type
 output "private_agents.disk_type" {
   description = "private agents.disk type"
-  value = "${module.private_agents.disk_type}"
+  value       = "${module.private_agents.disk_type}"
 }
 
 # Deployed private agent machine type
 output "private_agents.machine_type" {
   description = "private agents.machine type"
-  value = "${module.private_agents.machine_type}"
+  value       = "${module.private_agents.machine_type}"
 }
 
 # Deployed private agent image
 output "private_agents.image" {
   description = "private agents.image"
-  value = "${module.private_agents.image}"
+  value       = "${module.private_agents.image}"
 }
 
 # Deployed private agent SSH user
 output "private_agents.ssh_user" {
   description = "private agents.ssh user"
-  value = "${module.private_agents.ssh_user}"
+  value       = "${module.private_agents.ssh_user}"
 }
 
 # Returns the ID of the prereq script (if image are not used)
 output "private_agents.prereq_id" {
   description = "private agents.prereq id"
-  value = "${module.private_agents.prereq_id}"
+  value       = "${module.private_agents.prereq_id}"
 }
 
 # Number of public agents
 output "num_public_agents" {
   description = "Specify the amount of public agents. These agents will host marathon-lb and edgelb"
-  value = "${var.num_public_agents}"
+  value       = "${var.num_public_agents}"
 }
 
 # Deployed public agent disk size
 output "public_agents.disk_size" {
   description = "public agents.disk size"
-  value = "${module.public_agents.disk_size}"
+  value       = "${module.public_agents.disk_size}"
 }
 
 # Deployed public agent disk type
 output "public_agents.disk_type" {
   description = "public agents.disk type"
-  value = "${module.public_agents.disk_type}"
+  value       = "${module.public_agents.disk_type}"
 }
 
 # Deployed public agent machine type
 output "public_agents.machine_type" {
   description = "public agents.machine type"
-  value = "${module.public_agents.machine_type}"
+  value       = "${module.public_agents.machine_type}"
 }
 
 # provided public agent OS image
 output "public_agents.image" {
   description = "public agents.image"
-  value = "${module.public_agents.image}"
+  value       = "${module.public_agents.image}"
 }
 
 # Deployed public agent SSH user
 output "public_agents.ssh_user" {
   description = "public agents.ssh user"
-  value = "${module.public_agents.ssh_user}"
+  value       = "${module.public_agents.ssh_user}"
 }
 
 # Returns the ID of the prereq script (if image are not used)
 output "public_agents.prereq_id" {
   description = "public agents.prereq id"
-  value = "${module.public_agents.prereq_id}"
+  value       = "${module.public_agents.prereq_id}"
 }
 
 # Bootstrap private ip
 output "bootstrap.private_ip" {
   description = "Private IP of the bootstrap instance"
-  value = "${module.bootstrap.private_ip}"
+  value       = "${module.bootstrap.private_ip}"
 }
 
 # Bootstrap public ip
 output "bootstrap.public_ip" {
   description = "Public IP of the bootstrap instance"
-  value = "${module.bootstrap.public_ip}"
+  value       = "${module.bootstrap.public_ip}"
 }
 
 # masters public ip
 output "masters.public_ips" {
   description = "Master instances public IPs"
-  value = "${module.masters.public_ips}"
+  value       = "${module.masters.public_ips}"
 }
 
 # masters private ip
 output "masters.private_ips" {
   description = "Master instances private IPs"
-  value = "${module.masters.private_ips}"
+  value       = "${module.masters.private_ips}"
 }
 
 # private_agent public ip
 output "private_agents.public_ips" {
   description = "Private Agent public IPs"
-  value = "${module.private_agents.public_ips}"
+  value       = "${module.private_agents.public_ips}"
 }
 
 # private_agent private ip
 output "private_agents.private_ips" {
   description = "Private Agent instances private IPs"
-  value = "${module.private_agents.private_ips}"
+  value       = "${module.private_agents.private_ips}"
 }
 
 # public_agent public ip
 output "public_agents.public_ips" {
   description = "Private Agent public IPs"
-  value = "${module.public_agents.public_ips}"
+  value       = "${module.public_agents.public_ips}"
 }
 
 # public_agent private ip
 output "public_agents.private_ips" {
   description = "Private Agent instances private IPs"
-  value = "${module.public_agents.private_ips}"
+  value       = "${module.public_agents.private_ips}"
 }
 
 # Master node tested OSes image
 output "masters.dcos_instance_os" {
   description = "masters.dcos instance os"
-  value = "${module.masters.dcos_instance_os}"
+  value       = "${module.masters.dcos_instance_os}"
 }
 
 # Public Agent node tested OSes image
 output "public_agents.dcos_instance_os" {
   description = "public agents.dcos instance os"
-  value = "${module.public_agents.dcos_instance_os}"
+  value       = "${module.public_agents.dcos_instance_os}"
 }
 
 # Private agent node tested OSes image
 output "private_agents.dcos_instance_os" {
   description = "private agents.dcos instance os"
-  value = "${module.private_agents.dcos_instance_os}"
+  value       = "${module.private_agents.dcos_instance_os}"
 }
 
 # Bootstrap node tested OSes image
 output "bootstrap.dcos_instance_os" {
   description = "bootstrap.dcos instance os"
-  value = "${module.bootstrap.dcos_instance_os}"
+  value       = "${module.bootstrap.dcos_instance_os}"
 }
 
 # DCOS Version
 output "dcos_version" {
   description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
-  value = "${var.dcos_version}"
+  value       = "${var.dcos_version}"
 }
--- variables.tf
+++ variables.tf
@@ -6,163 +6,163 @@ variable "name_prefix" {
 # Master CIDR Range
 variable "master_cidr_range" {
   description = "master cidr range"
-  default = "10.64.0.0/16"
+  default     = "10.64.0.0/16"
 }
 
 # Agent CIDR Range
 variable "agent_cidr_range" {
   description = "agent cidr range"
-  default = "10.65.0.0/16"
+  default     = "10.65.0.0/16"
 }
 
 # Bootstrap node disk size (gb)
 variable "bootstrap_disk_size" {
   description = "bootstrap disk size"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap node disk type. Can be either "pd-ssd", "local-ssd", or "pd-standard".
 variable "bootstrap_disk_type" {
   description = "bootstrap disk type"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap node machine type
 variable "bootstrap_machine_type" {
   description = "[BOOTSTRAP] Machine type"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap node OS image
 variable "bootstrap_image" {
   description = "bootstrap image"
-  default = ""
+  default     = ""
 }
 
 # Master node disk size (gb)
 variable "master_disk_size" {
   description = "master disk size"
-  default = ""
+  default     = ""
 }
 
 # Master node disk type. Can be either "pd-ssd", "local-ssd", or "pd-standard".
 variable "master_disk_type" {
   description = "master disk type"
-  default = ""
+  default     = ""
 }
 
 # Master node machine type
 variable "master_machine_type" {
   description = "master machine type"
-  default = ""
+  default     = ""
 }
 
 # Master node OS image
 variable "master_image" {
   description = "master image"
-  default = ""
+  default     = ""
 }
 
 # Private agent node disk size (gb)
 variable "private_agent_disk_size" {
   description = "private agent disk size"
-  default = ""
+  default     = ""
 }
 
 # Private agent node disk type. Can be either "pd-ssd", "local-ssd", or "pd-standard".
 variable "private_agent_disk_type" {
   description = "private agent disk type"
-  default = ""
+  default     = ""
 }
 
 # Private agent node machine type
 variable "private_agent_machine_type" {
   description = "private agent machine type"
-  default = ""
+  default     = ""
 }
 
 # Private agent node OS image
 variable "private_agent_image" {
   description = "private agent image"
-  default = ""
+  default     = ""
 }
 
 # Public agent disk size (gb)
 variable "public_agent_disk_size" {
   description = "public agent disk size"
-  default = ""
+  default     = ""
 }
 
 # Public agent node disk type. Can be either "pd-ssd", "local-ssd", or "pd-standard".
 variable "public_agent_disk_type" {
   description = "public agent disk type"
-  default = ""
+  default     = ""
 }
 
 # Public agent machine type
 variable "public_agent_machine_type" {
   description = "public agent machine type"
-  default = ""
+  default     = ""
 }
 
 # Public agent node OS image
 variable "public_agent_image" {
   description = "public agent image"
-  default = ""
+  default     = ""
 }
 
 # Master node Public SSH Key
 variable "master_public_ssh_key_path" {
   description = "master public ssh key path"
-  default = ""
+  default     = ""
 }
 
 # Private Agent node Public SSH Key
 variable "private_agent_public_ssh_key_path" {
   description = "private agent public ssh key path"
-  default = ""
+  default     = ""
 }
 
 # Public Agent node Public SSH Key
 variable "public_agent_public_ssh_key_path" {
   description = "public agent public ssh key path"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap Node Public SSH Key
 variable "bootstrap_public_ssh_key_path" {
   description = "bootstrap public ssh key path"
-  default = ""
+  default     = ""
 }
 
 # Master node SSH User
 variable "master_ssh_user" {
   description = "master ssh user"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap node SSH User
 variable "bootstrap_ssh_user" {
   description = "bootstrap ssh user"
-  default = ""
+  default     = ""
 }
 
 # Public Agent node SSH User
 variable "public_agent_ssh_user" {
   description = "public agent ssh user"
-  default = ""
+  default     = ""
 }
 
 # Private Agent ndoe SSH User
 variable "private_agent_ssh_user" {
   description = "private agent ssh user"
-  default = ""
+  default     = ""
 }
 
 # Global Infra SSH User
 variable "infra_ssh_user" {
   description = "infra ssh user"
-  default = ""
+  default     = ""
 }
 
 # Global Infra Public SSH Key
@@ -173,49 +173,49 @@ variable "infra_public_ssh_key_path" {
 # Global Infra Disk Type
 variable "infra_disk_type" {
   description = "infra disk type"
-  default = "pd-ssd"
+  default     = "pd-ssd"
 }
 
 # Global Infra Disk Size
 variable "infra_disk_size" {
   description = "infra disk size"
-  default = "128"
+  default     = "128"
 }
 
 # Global Infra Machine Type
 variable "infra_machine_type" {
   description = "infra machine type"
-  default = "n1-standard-8"
+  default     = "n1-standard-8"
 }
 
 # Global Infra Tested OSes Image
 variable "infra_dcos_instance_os" {
   description = "infra dcos instance os"
-  default = "coreos_1576.5.0"
+  default     = "coreos_1576.5.0"
 }
 
 # Master node tested OSes image
 variable "master_dcos_instance_os" {
   description = "master dcos instance os"
-  default = ""
+  default     = ""
 }
 
 # Public Agent node tested OSes image
 variable "public_agent_dcos_instance_os" {
   description = "public agent dcos instance os"
-  default = ""
+  default     = ""
 }
 
 # Private agent node tested OSes image
 variable "private_agent_dcos_instance_os" {
   description = "private agent dcos instance os"
-  default = ""
+  default     = ""
 }
 
 # Bootstrap node tested OSes image
 variable "bootstrap_dcos_instance_os" {
   description = "bootstrap dcos instance os"
-  default = ""
+  default     = ""
 }
 
 variable "admin_ips" {
@@ -231,30 +231,30 @@ variable "admin_ips" {
 # Number of Masters
 variable "num_masters" {
   description = "Specify the amount of masters. For redundancy you should have at least 3"
-  default = "3"
+  default     = "3"
 }
 
 # Number of Private Agents
 variable "num_private_agents" {
   description = "Specify the amount of private agents. These agents will provide your main resources"
-  default = "1"
+  default     = "1"
 }
 
 # Number of Public Agents
 variable "num_public_agents" {
   description = "Specify the amount of public agents. These agents will host marathon-lb and edgelb"
-  default = "1"
+  default     = "1"
 }
 
 # DCOS Version
 variable "dcos_version" {
   description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
-  default = "1.11.4"
+  default     = "1.11.4"
 }
 
 # Add special tags to the resources created by this module
 variable "tags" {
   description = "Add custom tags to all resources"
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
 }
